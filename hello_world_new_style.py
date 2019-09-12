914f7c0bdea27dff8bd91aa8e790d4bffb509048
Ubuntu <ubuntu@ip-172-31-27-134.ec2.internal>
2019-09-12 18:49:14 +0000

    new code:

---
+++ hello_world_new_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Wajd Mariam       
+# Created on: September 2019
+# This is a new style to do the "Hello, World!" program
+
+def main():
+    print("Hello,World!")
+    
+
+if __name__ == "__main__":
+    main()
+    
